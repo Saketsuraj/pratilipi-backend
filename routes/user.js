9b56923024804d3befc92227b596ed84c67ca668
backendinternopenapp <backendintern@openapp.co>
2020-09-17 16:50:55 +0530

    Active users count

--- routes/user.js
+++ routes/user.js
@@ -11,7 +11,19 @@ const auth = require("../middleware/auth");
 const User = require("../model/User");
 const SaveStory = require("../model/SaveStory");
 const TotalCount = require("../model/TotalCount");
+const io = require('socket.io').listen(7777);
 
+var count = 0;
+
+io.sockets.on('connection', function(socket) {
+  count++;
+  io.sockets.emit('message', { count: count });
+
+  io.sockets.on('disconnect', function(){
+      count--;
+      io.sockets.emit('message', { count: count });
+  })
+});
 /**
  * @method - POST
  * @param - /signup
